938ab1d29e334eca214b613ac4b3ff5e1ee4c54f
God Chama <mar21430@uvg.edu.gt>
2023-07-15 16:14:11 -0600

    Update R_Workspace_Offline_Viewport.cpp

--- src/R_Workspace_Offline_Viewport.cpp
+++ src/R_Workspace_Offline_Viewport.cpp
@@ -33,15 +33,46 @@ void OpenGL_Preview::paintGL() {
 	glLoadIdentity();
 
 	if (Render) {
+		std::vector<float> Flattened_Pixmap;
 		for (int y = 0; y < Renderer->Pixmap.size(); y++) {
 			for (int x = 0; x < Renderer->Pixmap[0].size(); x++) {
 				Rgba& rgba = Renderer->Pixmap[y][x];
-				glColor4f(rgba.R, rgba.G, rgba.B, rgba.A);
-				glBegin(GL_POINTS);
-				glVertex2f(x, y);
-				glEnd();
+				Flattened_Pixmap.push_back(rgba.R);
+				Flattened_Pixmap.push_back(rgba.G);
+				Flattened_Pixmap.push_back(rgba.B);
+				Flattened_Pixmap.push_back(rgba.A);
 			}
 		}
+
+		int x = width() / Renderer->Pixmap[0].size();
+		int y = height() / Renderer->Pixmap.size();
+
+		glEnable(GL_TEXTURE_2D);
+
+		GLuint texture_id;
+		glGenTextures(1, &texture_id);
+		glBindTexture(GL_TEXTURE_2D, texture_id);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, x, y, 0, GL_RGBA, GL_FLOAT, Flattened_Pixmap.data());
+
+		glBegin(GL_QUADS);
+		glTexCoord2f(0, 0);
+		glVertex2f(-1, -1);
+		glTexCoord2f(1, 0);
+		glVertex2f(1, -1);
+		glTexCoord2f(1, 1);
+		glVertex2f(1, 1);
+		glTexCoord2f(0, 1);
+		glVertex2f(-1, 1);
+		glEnd();
+
+		glDeleteTextures(1, &texture_id);
+		glDisable(GL_TEXTURE_2D);
 	}
 }
 
@@ -61,17 +92,11 @@ Offline_Renderer::Offline_Renderer(QT_Text_Stream* P_Log, uint32_t P_ResX, uint3
 
 	for (int y = 0; y < P_ResY; y++) {
 		for (int x = 0; x < P_ResX; x++) {
-			Pixmap[y][x] = Rgba();
+			Pixmap[y][x] = Rgba(1,0,0,1);
 		}
 	}
 
 	std::stringstream log;
 	log << "X: " << Pixmap[0].size() << std::endl << "Y: " << Pixmap.size() << std::endl;
-	/*for (const auto& row : Pixmap) {
-		for (const auto& pixel : row) {
-			log << "R: " << pixel.R << ", " << "G: " << pixel.G << ", " << "B: " << pixel.B << ", " << "A: " << pixel.A << std::endl;
-		}
-		log << std::endl;
-	}*/
 	Log->log(log.str());
 }
